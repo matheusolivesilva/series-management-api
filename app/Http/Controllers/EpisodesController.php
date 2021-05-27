adef89a44f21b0044a2bd94c2611a6d9bb0fc148
Matheus Oliveira da Silva <matheus.oliveira.silv@gmail.com>
2021-05-26 22:57:52 -0300

    refactor: create BaseController for all controllers

--- app/Http/Controllers/EpisodesController.php
+++ app/Http/Controllers/EpisodesController.php
@@ -2,55 +2,12 @@
 namespace App\Http\Controllers;
 
 use App\Models\Episode;
-use Illuminate\Http\Request;
 
-class EpisodesController
+class EpisodesController extends BaseController
 {
-    public function index()
+    public function __construct()
     {
-        return Episode::all();
-    }
-
-    public function store(Request $request)
-    {
-        return response()
-            ->json(
-                Episode::create($request->all()),
-                201
-            );
-    }
-
-    public function show(int $id)
-    {
-        $serie = Episode::find($id);
-        if(is_null($serie)) {
-            return response()->json('', 204);
-        }
-        return response()->json($serie, 200);
-    }
-
-    public function update(int $id, Request $request)
-    {
-        $serie = Episode::find($id);
-        if(is_null($serie)) {
-            return response()->json([
-                'error' => 'Resource not found'
-            ], 404);
-        }
-        $serie->fill($request->all());
-        $serie->save();
-        return $serie;
-    }
-
-    public function destroy(int $id)
-    {
-        $amountOfDeletedEpisodes = Episode::destroy($id);
-        if($amountOfDeletedEpisodes === 0) {
-            return response()->json([
-                'error' => 'Resource not found'
-            ], 404);
-        }
-        return response()->json('', 204);
+       $this->class = Episode::class;
     }
 }
 
