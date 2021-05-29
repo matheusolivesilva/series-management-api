5aab791e20b687def202a801e585bd811e786518
Matheus Oliveira da Silva <matheus.oliveira.silv@gmail.com>
2021-05-29 00:06:10 -0300

    feat: create pagination for series

---
+++ app/Http/Controllers/BaseController.php
@@ -0,0 +1,57 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+abstract class BaseController
+{
+    protected $class;
+
+    public function index(Request $request)
+    {
+        return $this->class::paginate($request->per_page);
+    }
+
+    public function store(Request $request)
+    {
+        return response()
+            ->json(
+                $this->class::create($request->all()),
+                201
+            );
+    }
+
+    public function show(int $id)
+    {
+        $class = $this->class::find($id);
+        if(is_null($class)) {
+            return response()->json('', 204);
+        }
+        return response()->json($class, 200);
+    }
+
+    public function update(int $id, Request $request)
+    {
+        $class = $this->class::find($id);
+        if(is_null($class)) {
+            return response()->json([
+                'error' => 'Resource not found'
+            ], 404);
+        }
+        $class->fill($request->all());
+        $class->save();
+        return $class;
+    }
+
+    public function destroy(int $id)
+    {
+        $amountOfDeletedObjects = $this->class::destroy($id);
+        if($amountOfDeletedObjects === 0) {
+            return response()->json([
+                'error' => 'Resource not found'
+            ], 404);
+        }
+        return response()->json('', 204);
+    }
+}
+
